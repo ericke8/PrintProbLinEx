698c483adb92ba2d14491b12dab3e13a1d707fab
Jason Vega <jasonvega14@yahoo.com>
2020-04-21 15:23:36 -0700

    A bit buggy but kinda works

--- ocrd.py
+++ ocrd.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import numpy as np
 import xml.etree.ElementTree as et
+from math import atan2
 
 PAGE_TAG = "ns0:Page"
 TEXTREGION_TAG = "ns0:TextRegion"
@@ -21,8 +22,7 @@ UPPER_BINARY_THRESH = 255
 
 COORD_DELIM = ","
 
-GT_LINE_COLOR = (0, 255, 0)
-PRED_LINE_COLOR = (255, 0, 0)
+THICKNESS = 3
 LINES_CLOSED = True
 
 def get_line_coords(page, namespace):
@@ -50,8 +50,8 @@ def output_image(image, gt_lines):
     # Draw each ground truth bounding box on image
     for line_coords in gt_lines:
         line_coords = np.array(line_coords, np.int32)
-        image = cv2.polylines(image, [line_coords], LINES_CLOSED,
-                GT_LINE_COLOR, thickness=10)
+        #image = cv2.polylines(image, [line_coords], True, (0, 255, 0), 2)
+        #continue
         centroid = np.mean(line_coords, axis=0, dtype=np.uint32) 
         first_quad = []
         second_quad = []
@@ -79,11 +79,54 @@ def output_image(image, gt_lines):
         top_left_corner = second_quad[np.argmax(second_quad_distances)]
         bottom_left_corner = third_quad[np.argmax(third_quad_distances)]
         bottom_right_corner = fourth_quad[np.argmax(fourth_quad_distances)]
-        
-        cv2.line(image, (centroid[0], centroid[1]), (top_right_corner[0], top_right_corner[1]), (255, 0, 0), 3)
-        cv2.line(image, (centroid[0], centroid[1]), (top_left_corner[0], top_left_corner[1]), (255, 0, 0), 3)
-        cv2.line(image, (centroid[0], centroid[1]), (bottom_left_corner[0], bottom_left_corner[1]), (255, 0, 0), 3)
-        cv2.line(image, (centroid[0], centroid[1]), (bottom_right_corner[0], bottom_right_corner[1]), (255, 0, 0), 3)
+
+        top_line_angle_range = [atan2(-(top_right_corner - centroid)[1], (top_right_corner - centroid)[0]),
+                atan2(-(top_left_corner - centroid)[1], (top_left_corner - centroid)[0])]
+        right_line_angle_range = [atan2(-(bottom_right_corner - centroid)[1], (bottom_right_corner - centroid)[0]),
+                atan2(-(top_right_corner - centroid)[1], (top_right_corner - centroid)[0])]
+        bottom_line_angle_range = [atan2(-(bottom_left_corner - centroid)[1], (bottom_left_corner - centroid)[0]),
+                atan2(-(bottom_right_corner - centroid)[1], (bottom_right_corner - centroid)[0])]
+
+
+        top_line = [[top_left_corner[0], top_left_corner[1]], [top_right_corner[0], top_right_corner[1]]]
+        right_line = [[top_right_corner[0], top_right_corner[1]], [bottom_right_corner[0], bottom_right_corner[1]]]
+        bottom_line = [[bottom_right_corner[0], bottom_right_corner[1]], [bottom_left_corner[0], bottom_left_corner[1]]]
+        left_line = [[bottom_left_corner[0], bottom_left_corner[1]], [top_left_corner[0], top_left_corner[1]]]
+
+        for line_coord in line_coords:
+            angle = atan2(-(line_coord - centroid)[1], (line_coord - centroid)[0])
+
+            if (line_coord == top_left_corner).all() or (line_coord == top_right_corner).all() or \
+                    (line_coord == bottom_right_corner).all() or (line_coord == bottom_left_corner).all():
+                continue
+
+            if angle >= top_line_angle_range[0] and angle <= top_line_angle_range[1]:
+                top_line.append([line_coord[0], line_coord[1]])
+            
+            if angle >= right_line_angle_range[0] and angle <= right_line_angle_range[1]:
+                right_line.append([line_coord[0], line_coord[1]])
+            
+            if angle >= bottom_line_angle_range[0] and angle <= bottom_line_angle_range[1]:
+                bottom_line.append([line_coord[0], line_coord[1]])
+            
+            if angle <= bottom_line_angle_range[0] or angle >= top_line_angle_range[1]:
+                left_line.append([line_coord[0], line_coord[1]])
+
+        top_line = sorted(top_line, key=lambda k: [k[0], k[1]])
+        #right_line = sorted(right_line, key=lambda k: [k[1], k[0]])
+        bottom_line = sorted(bottom_line, key=lambda k: [k[0], k[1]])
+        #left_line = sorted(left_line, key=lambda k: [k[1], k[0]])
+
+        if len(line_coords) == 4:
+            cv2.line(image, (top_left_corner[0], top_left_corner[1]), (top_right_corner[0], top_right_corner[1]), (255, 0, 0), THICKNESS)
+            #cv2.line(image, (top_right_corner[0], top_right_corner[1]), (bottom_right_corner[0], bottom_right_corner[1]), (0, 255, 0), THICKNESS)
+            cv2.line(image, (bottom_right_corner[0], bottom_right_corner[1]), (bottom_left_corner[0], bottom_left_corner[1]), (0, 0, 255), THICKNESS)
+            #cv2.line(image, (bottom_left_corner[0], bottom_left_corner[1]), (top_left_corner[0], top_left_corner[1]), (0, 255, 255), THICKNESS)
+        else:
+            image = cv2.polylines(image, [np.array(top_line, np.int32)], not LINES_CLOSED, (255, 0, 0), thickness=THICKNESS)
+            #image = cv2.polylines(image, [np.array(right_line, np.int32)], not LINES_CLOSED, (0, 255, 0), thickness=THICKNESS)
+            image = cv2.polylines(image, [np.array(bottom_line, np.int32)], not LINES_CLOSED, (0, 0, 255), thickness=THICKNESS)
+            #image = cv2.polylines(image, [np.array(left_line, np.int32)], not LINES_CLOSED, (0, 255, 255), thickness=THICKNESS)
 
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
